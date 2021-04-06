ecfd5643a5fa340b3af6420ce26924eb3c658fdd
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-04-05 21:33:17 -0300

    feat: add error view to status 400

--- lib/exmall_url_web/views/error_view.ex
+++ lib/exmall_url_web/views/error_view.ex
@@ -1,6 +1,10 @@
 defmodule ExmallUrlWeb.ErrorView do
   use ExmallUrlWeb, :view
 
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
+  alias Ecto.Changeset
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.html", _assigns) do
@@ -12,5 +16,21 @@ defmodule ExmallUrlWeb.ErrorView do
   # "Not Found".
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
+  end
+
+  def render("400.json", %{result: %Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
+
+  # def render("400.json", %{result: message}) do
+  #   %{message: message}
+  # end
+
+  def translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
