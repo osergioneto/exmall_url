a75ec7be086a8642e8029ff339dc9e1979fd8eb2
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-04-06 23:33:16 -0300

    test: add test to check invalid params on user creation

--- test/exmall_url/users/create_test.exs
+++ test/exmall_url/users/create_test.exs
@@ -13,5 +13,17 @@ defmodule ExmallUrl.Users.CreateTest do
       assert %{ id: user_id, name: "Gaia Regina", email: "gaia@email.com"} == %{id: user_id, name: name, email: email}
     end
 
+    test "when there are invalid params, returns an error" do
+      params = %{name: "Gaia Regina", password: "123"}
+
+      {:error, changeset} = Create.call(params)
+
+      expected_errors = %{
+          email: ["can't be blank"],
+          password: ["should be at least 6 character(s)"]
+      }
+
+      assert errors_on(changeset) == expected_errors
+    end
   end
 end
