ef41483e02a5ea4c14cd973b80ab5627192c1927
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-05-04 00:44:39 -0300

    feat: add module to create short url

---
+++ lib/exmall_url/links/create.ex
@@ -0,0 +1,17 @@
+defmodule ExmallUrl.Links do
+  alias Ecto
+  alias ExmallUrl.{Link, Repo}
+
+  def call(params) do
+    params
+    |> generate_path()
+    |> Link.changeset()
+    |> Repo.insert()
+  end
+
+  def generate_path(%{"path" => path} = params) when path == "",
+    do: %{params | "path" => Ecto.UUID.generate()}
+
+  def generate_path(params),
+    do: params
+end
