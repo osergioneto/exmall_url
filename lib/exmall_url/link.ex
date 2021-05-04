147e6faceba97964feafa302d84d22e2cbe1f77a
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-05-03 23:41:00 -0300

    feat: add path and url regex

--- lib/exmall_url/link.ex
+++ lib/exmall_url/link.ex
@@ -7,6 +7,8 @@ defmodule ExmallUrl.Link do
 
   @required_params [:path, :to_url]
   @optional_params [:user_id]
+  @path_regex ~r/^[a-z0-9]+(?:-[a-z0-9]+)*$/i
+  @url_regex ~r/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~?#&\/\/=]*)/
 
   schema "links" do
     field :path, :string
@@ -20,10 +22,8 @@ defmodule ExmallUrl.Link do
     link
     |> cast(params, @required_params)
     |> validate_required(@required_params)
-    # add regex
-    |> validate_format(:path, ~r{a})
-    # add regex
-    |> validate_format(:to_url, ~r{a})
+    |> validate_format(:path, @path_regex)
+    |> validate_format(:to_url, @url_regex)
     |> unique_constraint(:path)
   end
 end
