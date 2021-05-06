aad3820a7fa7e949b986e487c02d0f18b535b95c
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-05-06 01:54:50 -0300

    test: add test to link create

---
+++ test/exmall_url/links/create_test.exs
@@ -0,0 +1,40 @@
+defmodule ExmallUrl.Links.CreateTest do
+  use ExmallUrl.DataCase, async: true
+
+  alias ExmallUrl.Link
+  alias ExmallUrl.Links.Create
+
+  describe "Links.call/1" do
+    test "when all params are valid, return an link" do
+      params = %{
+        path: "google",
+        to_url: "https://www.google.com.br"
+      }
+
+      assert {:ok, %{path: "google", to_url: "https://www.google.com.br", user_id: nil}} =
+               Create.call(params)
+    end
+
+    test "when path is not send, return errors" do
+      params = %{
+        to_url: "https://hexdocs.pm/ecto"
+      }
+
+      assert {:error,
+              %Ecto.Changeset{errors: [path: {"can't be blank", [validation: :required]}]}} =
+               Create.call(params)
+    end
+
+    test "when path is empty string, return link with path as UUID" do
+      params = %{
+        "path" => "",
+        "to_url" => "https://hexdocs.pm/ecto"
+      }
+
+      {:ok, link} = Create.call(params)
+
+      assert "https://hexdocs.pm/ecto" == link.to_url
+      assert {:ok, link.path} == Ecto.UUID.cast(link.path)
+    end
+  end
+end
