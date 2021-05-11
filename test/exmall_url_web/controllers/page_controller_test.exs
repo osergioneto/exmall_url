2134992ebf145932ce9053e5c391f0953cfb15ea
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-05-10 22:07:33 -0300

    test: refactor test from homepage

--- test/exmall_url_web/controllers/page_controller_test.exs
+++ test/exmall_url_web/controllers/page_controller_test.exs
@@ -3,7 +3,7 @@ defmodule ExmallUrlWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Shorten Any Link"
   end
 
   # TODO: Add test to verify fallback controller
